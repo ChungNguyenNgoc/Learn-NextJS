d74e3ab4d6c95c8247638c7c491466cb9d6a9a83
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2022-07-30 14:12:25 +0700

    update code

---
+++ pages/post/[...params].tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const ParamsPage = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>ParamsPage</h1>
+
+      <p>Query: {JSON.stringify(router.query)}</p>
+    </div>
+  );
+};
+
+export default ParamsPage;
---
+++ pages/post/[postId].tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+import React from "react";
+
+const PostDetailPage = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>PostDetailPage</h1>
+
+      <p>Query: {JSON.stringify(router.query)}</p>
+    </div>
+  );
+};
+
+export default PostDetailPage;
