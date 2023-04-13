e27cccc776282842a16e9870609c9ead68807ac8
NguyenNgocBaoChung <chung.nnb@alta.com.vn>
2023-04-13 16:02:23 +0700

    feat: setup prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.css";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const [code, setCode] = useState("Chung");
+  const [code, setCode] = useState('Chung');
   const router = useRouter();
 
   useEffect(() => {
@@ -16,10 +16,10 @@ const Home: NextPage = () => {
 
   function goToDetailPage() {
     router.push({
-      pathname: "/post/[postId]",
+      pathname: '/post/[postId]',
       query: {
         postId: 123,
-        ref: "social",
+        ref: 'social',
       },
     });
   }
@@ -38,8 +38,7 @@ const Home: NextPage = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started by editing <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <button onClick={goToDetailPage}>Go to Detail page</button>
@@ -55,10 +54,7 @@ const Home: NextPage = () => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -68,13 +64,11 @@ const Home: NextPage = () => {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
 
-        <div style={{ marginTop: "1200px" }}></div>
+        <div style={{ marginTop: '1200px' }}></div>
 
         <Link href="/about">
           <a>
@@ -89,7 +83,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
